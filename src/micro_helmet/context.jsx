6d7ff4d08e4026a72591c38c0fbc3c1fd4a0303f
Max Degterev <max@degterev.me>
2020-11-20 18:34:12 +0100

    lint fix

--- src/micro_helmet/context.jsx
+++ src/micro_helmet/context.jsx
@@ -1,8 +1,10 @@
 import { createContext } from 'react';
 
+const addProps = () => { console.warn('Please use MicroHelmet Provider'); };
 
-export default createContext({
-  addProps() {
-    console.warn('Please use MicroHelmet Provider');
-  },
-});
+const MicroHelmetContext = createContext({ addProps });
+
+export const Consumer = MicroHelmetContext.Consumer;
+export const Provider = MicroHelmetContext.Provider;
+
+export default MicroHelmetContext;
