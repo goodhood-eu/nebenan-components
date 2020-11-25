c3a6933ab0e89c8dbbb28941ec0d9b2964772911
Peter Haßler <34499633+peterhass@users.noreply.github.com>
2020-11-25 13:00:36 +0100

    Add `onClick` capability to LinkHeader (#52)
    * Add `onClick` capability to LinkHeader
    
    * Pass on onClick to Link to not break usages of onClick + to
    
    * Bump package version

--- src/link_header/index.jsx
+++ src/link_header/index.jsx
@@ -4,25 +4,50 @@ import clsx from 'clsx';
 import { Link } from 'react-router-dom';
 
 
-const LinkHeader = (props) => {
-  const { children, to, reversed, ...cleanProps } = props;
-  const className = clsx('c-link_header', props.className, {
+const LinkHeader = ({
+  children,
+  to,
+  onClick,
+  reversed,
+  className: passedClassName,
+  ...cleanProps
+}) => {
+  const isClickable = to || onClick;
+
+  const className = clsx('c-link_header', passedClassName, {
     'is-reversed': reversed,
+    'is-clickable': isClickable,
   });
 
-  if (!to) return <span {...cleanProps} className={className}>{children}</span>;
+  let icon;
+  if (isClickable) {
+    const iconClass = clsx({
+      'icon-arrow_right': !reversed,
+      'icon-arrow_left': reversed,
+    });
 
-  const iconClass = clsx({
-    'icon-arrow_right': !reversed,
-    'icon-arrow_left': reversed,
-  });
-  const icon = <i className={iconClass} />;
-  return <Link {...cleanProps} {...{ to, className }}>{icon}{children}</Link>;
+    icon = <i className={iconClass} />;
+  }
+
+  if (!to) {
+    return (
+      <span
+        {...cleanProps}
+        onClick={onClick}
+        className={className}
+      >
+        {icon}
+        {children}
+      </span>
+    );
+  }
+  return <Link {...cleanProps} {...{ to, className, onClick }}>{icon}{children}</Link>;
 };
 
 LinkHeader.propTypes = {
   className: PropTypes.string,
   to: PropTypes.string,
+  onClick: PropTypes.func,
   reversed: PropTypes.bool,
   children: PropTypes.node,
 };
