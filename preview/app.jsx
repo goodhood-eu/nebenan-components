d9762a2c312a92f96b6fa1bd89d173e159816307
eliasbraun <elias.braun@nebenan.de>
2023-04-17 14:58:03 +0200

    Update to react 18

--- preview/app.jsx
+++ preview/app.jsx
@@ -1,6 +1,25 @@
-import { hydrate } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-
+import { createRoot, hydrateRoot } from 'react-dom/client';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import createRouter from './router';
 
-hydrate(<BrowserRouter>{createRouter()}</BrowserRouter>, document.getElementById('main'));
+
+const mainNode = document.getElementById('main');
+const isPrerender = Boolean(mainNode);
+
+if (isPrerender) {
+  hydrateRoot(mainNode, (
+    <BrowserRouter>
+      {createRouter()}
+    </BrowserRouter>
+  ));
+} else {
+  const node = document.createElement('main');
+  document.body.appendChild(node);
+
+  createRoot(node).render((
+    // eslint-disable-next-line no-undef
+    <HashRouter basename={PUBLIC_PATH}>
+      {createRouter()}
+    </HashRouter>
+  ));
+}
